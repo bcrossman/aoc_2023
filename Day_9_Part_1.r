75ec54d43d97539add2fab2e419512a1af2598d3
Brent Crossman <brent.crossman@gmail.com>
2023-12-09 08:06:33 -0500

    day 9

---
+++ Day_9_Part_1.r
@@ -0,0 +1,41 @@
+library(tidyverse)
+library(unglue)
+# Prep
+
+file <- readLines("./Day_9/Part_1/input.txt")
+
+readings <- (lapply(file, function(x) parse_number(unlist(strsplit(x, " ")))))
+
+results <- list()
+count <- 1
+for(i in readings){
+  # print(i)
+  # i <- readings[[2]]
+  x_values <- 1:length(i)
+  degree <- length(x_values)-1
+  fit <- lm(i ~ poly(x_values, degree))
+  next_x <- max(x_values) + 1
+  predicted_value <- predict(fit, newdata = data.frame(x_values = next_x))[[1]]
+  results[[as.character(count)]] <- predicted_value
+  count <- count+1
+  
+}
+results %>% unlist() %>% sum()
+
+## Part 2
+
+results <- list()
+count <- 1
+for(i in readings){
+  # print(i)
+  # i <- readings[[2]]
+  x_values <- 1:length(i)
+  degree <- length(x_values)-1
+  fit <- lm(i ~ poly(x_values, degree))
+  next_x <- 0  ## Only Change
+  predicted_value <- predict(fit, newdata = data.frame(x_values = next_x))[[1]]
+  results[[as.character(count)]] <- predicted_value
+  count <- count+1
+  
+}
+results %>% unlist() %>% sum() %>% round()
